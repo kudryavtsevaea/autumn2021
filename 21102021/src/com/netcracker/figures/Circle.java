58c1c644d728207c3ab8712aaf734024b2e514e5
Elena <elenakudravceva2015@gmail.com>
2021-10-22 21:35:31 +0400

    Task with circle's point

---
+++ 21102021/src/com/netcracker/figures/Circle.java
@@ -0,0 +1,9 @@
+package com.netcracker.figures;
+
+public class Circle {
+
+    public Circle(int x, int y) {
+        Point point = new Point(x, y);
+        System.out.println("x = " + x + "\ny = " + y);
+    }
+}
