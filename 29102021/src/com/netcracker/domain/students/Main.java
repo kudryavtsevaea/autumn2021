18c979236beadb6289426c2efae5e841894566a0
Elena <elenakudravceva2015@gmail.com>
2021-10-28 21:51:44 +0400

    student/master classes

---
+++ 29102021/src/com/netcracker/domain/students/Main.java
@@ -0,0 +1,10 @@
+package com.netcracker.domain.students;
+
+public class Main {
+    public static void main(String[] args) {
+      Student student = new Student("huihiuhuih");
+      Master master = new Master("IVAN IVANOVICH IVANOV", 124356L, "KNIIT", "KNIIT");
+      System.out.println(student);
+      System.out.println(master);
+    }
+}
